868cda009e84791535be2ec2b7585a5272179225
Brett Nash <nash@nash.id.au>
2010-04-22 17:58:38 +0800

    Basic working libensure.

--- src/ensure.c
+++ src/ensure.c
@@ -1,8 +1,11 @@
 #include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/signalfd.h>
 
 
 #include <Eina.h>
@@ -15,9 +18,22 @@ struct error {
 };
 
 
-Evas_Object *window_add(void);
+Evas_Object *window_add(char **argv);
 
 static void select_cb(void *data, Evas_Object *obj, void *event);
+
+static void on_run(void *data, Evas_Object *button, void *event_info);
+
+static void on_check(void *data, Evas_Object *button, void *event_info);
+
+static void dochild(char **args);
+
+static int signal_init(void);
+static int signalfd_child(void *data, Ecore_Fd_Handler *fd_handler);
+
+static Evas_Object *runbutton;
+static Evas_Object *checkbutton;
+static pid_t childid;
 
 int
 elm_main(int argc, char **argv){
@@ -27,7 +43,10 @@ elm_main(int argc, char **argv){
                printf("Usage: %s <program>\n", argv[0]);
                 exit(0);
         }
-        win = window_add();
+        win = window_add(argv);
+
+	signal_init();
+
         elm_list_go(win);
         elm_run();
         elm_shutdown();
@@ -38,8 +57,8 @@ ELM_MAIN()
 
 
 Evas_Object *
-window_add(void){
-        Evas_Object *win,*bg,*li,*bx,*run,*cfg;
+window_add(char **args){
+        Evas_Object *win,*bg,*li,*bx,*ctrls, *run, *check;
 
         win = elm_win_add(NULL, "Ensure", ELM_WIN_BASIC);
         elm_win_title_set(win, "Ensure");
@@ -52,23 +71,33 @@ window_add(void){
 
         bx = elm_box_add(win);
         evas_object_size_hint_weight_set(bx, 1.0, 1.0);
-       elm_win_resize_object_add(win, bg);
+	elm_win_resize_object_add(win, bg);
         evas_object_show(bg);
 
-        bx = elm_box_add(win);
-        evas_object_size_hint_weight_set(bx, 1.0, 1.0);
-        elm_win_resize_object_add(win, bx);
-        evas_object_show(bx);
+        ctrls = elm_box_add(bx);
+	elm_box_horizontal_set(ctrls, true);
+        evas_object_size_hint_weight_set(ctrls, 1.0, 0.0);
+        evas_object_show(ctrls);
 
+	/* Buttons: Main event is the 'clicked' event */
+	run = elm_button_add(ctrls);
+	elm_button_label_set(run, "Run");
+	elm_button_autorepeat_set(run, false);
+	elm_box_pack_end(ctrls,run);
+	evas_object_show(run);
+	evas_object_smart_callback_add(run, "clicked", on_run, args + 1);
+	runbutton = run;
 
-  /* add a label widget, set the text and put it in the pad frame */
+	check = elm_button_add(ctrls);
+	elm_button_label_set(check, "Check");
+	elm_button_autorepeat_set(check, false);
+	elm_object_disabled_set(check, true);
+	elm_box_pack_end(ctrls,check);
+	evas_object_show(check);
+	evas_object_smart_callback_add(check, "clicked", on_check, NULL);
+	checkbutton = check;
 
-   lb = elm_label_add(win);
-   elm_label_label_set(lb, "Books");
-   elm_box_pack_end(bx,lb);
-   evas_object_show(lb);
-
-        li = elm_list_add(win);
+        li = elm_list_add(bx);
         evas_object_size_hint_weight_set(li, 1, 1);
         evas_object_size_hint_align_set(li, -1, -1);
         elm_box_pack_end(bx, li);
@@ -78,7 +107,109 @@ evas_object_resize(win, 300, 300);
 evas_object_show(win);
         /* Urh.. the list widget for now */
         return li;
+
 }
+
+/**
+ * Initialise our signal handler
+ */
+static int
+signal_init(void){
+	int fd;
+	sigset_t sigchld;
+
+	sigemptyset(&sigchld);
+	sigaddset(&sigchld, SIGCHLD);
+
+	fd = signalfd(-1, &sigchld, SFD_CLOEXEC | SFD_NONBLOCK);
+	ecore_main_fd_handler_add(fd,ECORE_FD_READ|ECORE_FD_ERROR,
+			signalfd_child, NULL,
+			NULL, NULL);
+
+	return 0;
+}
+
+static int
+signalfd_child(void *data ensure_unused, Ecore_Fd_Handler *fdh){
+	int fd;
+	struct signalfd_siginfo siginfo;
+	printf("Child exited\n");
+
+	fd = ecore_main_fd_handler_fd_get(fdh);
+
+	read(fd, &siginfo, sizeof(struct signalfd_siginfo));
+
+	elm_object_disabled_set(runbutton, false);
+	elm_object_disabled_set(checkbutton, true);
+
+	return 1;
+}
+
+
+
+
+
+/**
+ * The user clicked the 'check' button: Start checking the application.
+ */
+static void
+on_check(void *data, Evas_Object *button, void *event_info ensure_unused){
+	printf("Doing check!!\n");
+
+	kill(childid, SIGUSR2);
+
+}
+
+
+
+
+
+/**
+ * The user clicked the 'run' button: Start running the application.
+ */
+static void
+on_run(void *data, Evas_Object *button, void *event_info ensure_unused){
+	pid_t pid;
+	char **args = data;
+
+	elm_object_disabled_set(runbutton, true);
+	elm_object_disabled_set(checkbutton, false);
+
+	printf("Running %s\n", args[0]);
+
+	/* I'm sure someone will complain I'm doing this myself... but anyway
+	 */
+	switch ((pid = fork())){
+	case -1:
+		/* FIXME: error message handling */
+		fprintf(stderr,"Unable to fork\n");
+		exit(7);
+		break;
+	case 0:
+		dochild(args);
+		exit(7);
+	default:
+		childid = pid;
+		/* Parent */
+		break;
+	}
+}
+
+/**
+ * Run the child process
+ */
+static void
+dochild(char **args){
+	/* FIXME: Should append: Not overwrite */
+	setenv("LD_PRELOAD", "./libensure.so.0",1);
+	execvp(args[0],args);
+	perror("execvp");
+	exit(7);
+}
+
+
+/* Signal handler for sig child */
+
 static void
 select_cb(void *data ensure_unused, Evas_Object *obj ensure_unused, void *event){
         /* FIXME: We should use a peruse pointer here */
@@ -88,5 +219,8 @@ select_cb(void *data ensure_unused, Evas_Object *obj ensure_unused, void *event)
 
         err = elm_list_item_data_get(itm);
 }
+
+
+
 
 
---
+++ src/libensure.c
@@ -0,0 +1,128 @@
+#include <limits.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/signalfd.h>
+#include <unistd.h>
+
+
+#include <Eina.h>
+#include <Ecore.h>
+#include <Ecore_Evas.h>
+#include <Evas.h>
+
+
+#define ensure_unused	__attribute__((unused))
+
+static int libensure_dump(void *data ensure_unused, Ecore_Fd_Handler *fdh);
+static void libensure_objdump(Evas_Object *o, Evas_Object *parent);
+
+struct Ecore_Evas_Wrap {
+	EINA_INLIST;
+};
+
+extern struct Ecore_Evas_Wrap *ecore_evases;
+
+FILE *outfile;
+
+__attribute__((constructor)) void
+libensure_init(void){
+	int fd;
+	sigset_t sigusr2;
+
+	ecore_init();
+
+	sigemptyset(&sigusr2);
+	sigaddset(&sigusr2, SIGUSR2);
+
+	fd = signalfd(-1, &sigusr2, SFD_CLOEXEC | SFD_NONBLOCK);
+	ecore_main_fd_handler_add(fd,ECORE_FD_READ|ECORE_FD_ERROR,
+			libensure_dump, NULL,
+			NULL, NULL);
+	outfile = stdout;
+	fprintf(outfile,"ensure init\n");
+
+	return;
+}
+
+
+static int
+libensure_dump(void *data ensure_unused, Ecore_Fd_Handler *fdh){
+	int fd;
+	struct signalfd_siginfo siginfo;
+	Eina_List *os,*ot;
+	Evas_Object *o;
+	struct Ecore_Evas_Wrap *eew;
+
+	fd = ecore_main_fd_handler_fd_get(fdh);
+
+	read(fd, &siginfo, sizeof(struct signalfd_siginfo));
+
+	fprintf(outfile,"Ensure dump!\n");
+
+	EINA_INLIST_FOREACH(ecore_evases, eew){
+		Evas *e;
+		e = ecore_evas_get((Ecore_Evas *)eew);
+		fprintf(outfile,"E: %p\n",e);
+		os = evas_objects_in_rectangle_get(e,SHRT_MIN, SHRT_MIN,
+				USHRT_MAX, USHRT_MAX, true, true);
+		EINA_LIST_FOREACH(os, ot, o){
+			libensure_objdump(o, NULL);
+		}
+
+	}
+
+	fprintf(outfile,"Ensure done\n");
+
+	return 1;
+}
+
+static void
+libensure_objdump(Evas_Object *o, Evas_Object *parent){
+	Eina_List *children, *tmp;
+	Evas_Object *child,*clip;
+	const char *type;
+	int x,y,w,h,a,r,g,b;
+
+	type = evas_object_type_get(o);
+	fprintf(outfile,"Object: %p '%s' ",o,type);
+
+
+	if (parent) fprintf(outfile,"Parent: %p ",parent);
+	evas_object_geometry_get(o,&x,&y,&w,&h);
+	fprintf(outfile,"Geometry: %+d%+d(%dx%d) ",x,y,w,h);
+	clip = evas_object_clip_get(o);
+	if (clip) fprintf(outfile, "Clip: %p ",parent);
+	evas_object_color_get(o, &r,&g,&b,&a);
+	fprintf(outfile, "Color (%d,%d,%d,%d) ",r,g,b,a);
+
+	/* Type specific things here */
+	if (strcmp(type, "image") == 0){
+		const char *file,*key;
+		evas_object_image_file_get(o,&file,&key);
+		if (key)
+			fprintf(outfile, "Image: '%s' (%s) ",file,key);
+		else
+			fprintf(outfile, "Image: '%s' ",file);
+	} else if (strcmp(type, "text") == 0){
+		fprintf(outfile,"Text: [[%s]] ",evas_object_text_text_get(o));
+	} else if (strcmp(type, "rectangle") == 0){
+
+	} else if (strcmp(type, "edje") == 0){
+
+	}
+
+
+	fprintf(outfile,"\n");
+
+	children = evas_object_smart_members_get(o);
+	EINA_LIST_FOREACH(children, tmp, child){
+		libensure_objdump(child,o);
+	}
+
+}
+
+__attribute__((destructor)) void
+libensure_fini(void){
+	ecore_shutdown();
+}
